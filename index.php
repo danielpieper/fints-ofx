dece7c5a86773f26f9bacc1a469efce7e5920144
Daniel Pieper <daniel.pieper@posterxxl.com>
2017-09-15 13:58:07 +0200

    Adds simple ofx output

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ use Assert\AssertionFailedException;
 use Fhp\FinTs;
 use Fhp\Model\StatementOfAccount\Statement;
 use Fhp\Model\StatementOfAccount\Transaction;
+use Lib\OFXWriter;
 
 $climate = new CLImate();
 $climate->description('Export bank account statements via fints to open finance exchange (ofx) files');
@@ -25,13 +26,11 @@ $climate->arguments->add([
         'prefix' => 'c',
         'longPrefix' => 'code',
         'description' => 'Bank routing code',
-        'castTo' => 'int',
     ],
     'account' => [
         'prefix' => 'a',
         'longPrefix' => 'account',
         'description' => 'Bank account number',
-        'castTo' => 'int',
     ],
     'user' => [
         'prefix' => 'u',
@@ -85,17 +84,22 @@ $fintsClient = new FinTs(
     $configuration['pin']
 );
 
-$sepaAccount = getSepaAccount($fintsClient);
+$sepaAccount = getSepaAccount($fintsClient, $configuration['account']);
 if (!$sepaAccount) {
+    $climate->error('Could not find bank sepa account.');
+    exit(1);
+}
+$account = getAccount($fintsClient, $configuration['account']);
+if (!$account) {
     $climate->error('Could not find bank account.');
     exit(1);
 }
 if ($configuration['verbose']) {
-    $padding = $climate->padding(10);
-    $padding->label('Account')->result($sepaAccount->getAccountNumber());
-    $padding->label('Owner')->result($sepaAccount->getAccountOwnerName());
-    $padding->label('Description')->result($sepaAccount->getAccountDescription());
-    $padding->label('IBAN')->result($sepaAccount->getIban());
+    $padding = $climate->padding(15);
+    $padding->label('Account')->result($account->getAccountNumber());
+    $padding->label('Owner')->result($account->getAccountOwnerName());
+    $padding->label('Description')->result($account->getAccountDescription());
+    $padding->label('IBAN')->result($account->getIban());
     $padding->label('Balance')->result($fintsClient->getSaldo($sepaAccount)->getAmount());
 }
 
@@ -104,11 +108,23 @@ $soa = $fintsClient->getStatementOfAccount(
     (new \DateTime($configuration['from'])),
     (new \DateTime($configuration['to']))
 );
+$ofxWriter = new OFXWriter();
+$ofxWriter->startDocument();
+$ofxWriter->writeSignOnMessageSet();
+$ofxWriter->startBankingMessageSet();
+$ofxWriter->startStatementTransactionWrapper();
+$ofxWriter->startStatementResponse($account);
+$ofxWriter->startStatementTransactionAggregate($soa);
 foreach ($soa->getStatements() as $statement) {
     foreach ($statement->getTransactions() as $transaction) {
-        // TODO
+         $ofxWriter->writeStatementTransaction($transaction);
     }
 }
+$ofxWriter->endStatementTransactionAggregate();
+$ofxWriter->endStatementResponse();
+$ofxWriter->endStatementTransactionWrapper();
+$ofxWriter->endBankingMessageSet();
+$ofxWriter->endDocument();
 
 function getConfiguration(CLImate &$climate)
 {
@@ -170,8 +186,8 @@ function getConfiguration(CLImate &$climate)
         Assert::lazy()
             ->that($configuration['url'], 'url')->notEmpty()->url()
             ->that($configuration['port'], 'port')->notEmpty()->integer()
-            ->that($configuration['code'], 'code')->notEmpty()->integer()
-            ->that($configuration['account'], 'account')->notEmpty()->integer()
+            ->that($configuration['code'], 'code')->notEmpty()
+            ->that($configuration['account'], 'account')->notEmpty()
             ->that($configuration['user'], 'user')->notEmpty()
             ->that($configuration['pin'], 'pin')->notEmpty()
             ->that($configuration['from'], 'from')->notEmpty()
@@ -184,12 +200,23 @@ function getConfiguration(CLImate &$climate)
     return $configuration;
 }
 
-function getSepaAccount(FinTs &$fintsClient)
+function getSepaAccount(FinTs &$fintsClient, $accountNumber)
 {
     $sepaAccounts = $fintsClient->getSEPAAccounts();
     foreach ($sepaAccounts as $sepaAccount) {
-        if ($sepaAccount->getAccountNumber() == $configuration['account']) {
+        if ($sepaAccount->getAccountNumber() == $accountNumber) {
             return $sepaAccount;
+        }
+    }
+    return false;
+}
+
+function getAccount(FinTs &$fintsClient, $accountNumber)
+{
+    $accounts = $fintsClient->getAccounts();
+    foreach ($accounts as $account) {
+        if ($account->getAccountNumber() == $accountNumber) {
+            return $account;
         }
     }
     return false;
---
+++ lib/OFXWriter.php
@@ -0,0 +1,126 @@
+<?php declare(strict_types=1);
+namespace Lib;
+
+use Fhp\Model\Account;
+use Fhp\Model\StatementOfAccount\StatementOfAccount;
+use Fhp\Model\StatementOfAccount\Transaction;
+
+class OFXWriter extends \XMLWriter
+{
+    public function __construct()
+    {
+        $this->openURI('php://stdout');
+        $this->setIndent(true);
+        $this->setIndentString('  ');
+    }
+
+    public function startDocument($version = '1.0', $encoding = 'UTF-8', $standalone = null)
+    {
+        parent::startDocument($version, $encoding, $standalone);
+        $this->writePi('OFX', 'OFXHEADER="200" VERSION="220" SECURITY="NONE" OLDFILEUID="NONE" NEWFILEUID="NONE"');
+        $this->startElement('OFX');
+    }
+
+    public function endDocument()
+    {
+        $this->endElement(); // OFX
+        parent::endDocument();
+    }
+
+    public function writeDateTimeElement($name, \DateTime $date)
+    {
+        $this->writeElement($name, $date->format('YmdHis'));
+    }
+
+    public function writeSignOnMessageSet()
+    {
+        $this->startElement('SIGNONMSGSRSV1');
+        $this->startElement('SONRS');
+
+        $this->startElement('STATUS');
+        $this->writeElement('CODE', '0');
+        $this->writeElement('SEVERITY', 'INFO');
+        $this->endElement(); // STATUS
+        $this->writeDateTimeElement('DTSERVER', new \DateTime('now'));
+        $this->writeElement('LANGUAGE', 'GER');
+
+        $this->endElement(); // SONRS
+        $this->endElement(); // SIGNONMSGSRSV1
+    }
+
+    public function startBankingMessageSet()
+    {
+        $this->startElement('BANKMSGSRSV1');
+    }
+
+    public function endBankingMessageSet()
+    {
+        $this->endElement(); // BANKMSGSRSV1
+    }
+
+    public function startStatementTransactionWrapper()
+    {
+        $this->startElement('STMTTRNRS');
+
+        $this->writeElement('TRNUID', '0');
+        $this->startElement('STATUS');
+        $this->writeElement('CODE', '0');
+        $this->writeElement('SEVERITY', 'INFO');
+        $this->endElement(); // STATUS
+    }
+
+    public function endStatementTransactionWrapper()
+    {
+        $this->endElement(); // STMTTRNRS
+    }
+
+    public function startStatementResponse(Account $account)
+    {
+        $this->startElement('STMTRS');
+        $this->writeElement('CURDEF', $account->getCurrency());
+
+        $this->startElement('BANKACCTFROM');
+        $this->writeElement('BANKID', (string)$account->getBankCode());
+        $this->writeElement('ACCTID', (string)$account->getAccountNumber());
+        $this->writeElement('ACCTTYPE', 'CHECKING');
+        $this->endElement(); // BANKACCTFROM
+    }
+
+    public function endStatementResponse()
+    {
+        $this->endElement(); // STMTRS
+    }
+
+    public function startStatementTransactionAggregate(StatementOfAccount $statementOfAccount)
+    {
+        $this->startElement('BANKTRANLIST');
+        $statements = $statementOfAccount->getStatements();
+        $statementCount = count($statements);
+
+        if ($statementCount > 0) {
+            $this->writeDateTimeElement('DTSTART', $statements[0]->getDate());
+            $this->writeDateTimeElement('DTEND', $statements[$statementCount - 1]->getDate());
+        }
+    }
+
+    public function endStatementTransactionAggregate()
+    {
+        $this->endElement(); // BANKTRANLIST
+    }
+
+    public function writeStatementTransaction(Transaction $transaction)
+    {
+        $this->startElement('STMTTRN');
+
+        $this->writeElement('TRNTYPE', strtoupper($transaction->getCreditDebit()));
+        $this->writeDateTimeElement('DTPOSTED', $transaction->getBookingDate());
+        $this->writeElement(
+            'TRNAMT',
+            ($transaction->getCreditDebit() == Transaction::CD_DEBIT ? '-' : '') . $transaction->getAmount()
+        );
+        $this->writeElement('FITID', md5($transaction->getDescription1()));
+        $this->writeElement('NAME', $transaction->getName());
+
+        $this->endElement(); // STMTTRN
+    }
+}
