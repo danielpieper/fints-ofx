18e2d34354fd9b0ff155b5a03869da3ec810556e
Daniel Pieper <daniel.pieper@oneclick-cloud.com>
2018-02-03 09:40:39 +0100

    Adds ofxwriter test

--- tests/OFXWriterTest.php
+++ tests/OFXWriterTest.php
@@ -2,9 +2,25 @@
 
 namespace FintsOfx;
 
+use danielpieper\FintsOfx\OFXWriter;
+use Money\Currencies\ISOCurrencies;
+use Money\Formatter\DecimalMoneyFormatter;
 use PHPUnit\Framework\TestCase;
 
 class OFXWriterTest extends TestCase
 {
+    public function test_it_can_create_documents(): void
+    {
+        $currencies = new ISOCurrencies();
+        $moneyFormatter = new DecimalMoneyFormatter($currencies);
+        $filename = tempnam(sys_get_temp_dir(), 'fintsofx');
 
+        $ofxWriter = new OFXWriter($filename, $moneyFormatter);
+        $ofxWriter->startDocument();
+        $ofxWriter->endDocument();
+
+        $actual = file_get_contents($filename);
+        $expected = file_get_contents(__DIR__ . '/../fixtures/empty_document.txt');
+        $this->assertSame($expected, $actual);
+    }
 }
